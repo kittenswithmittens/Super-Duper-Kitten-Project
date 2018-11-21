0df447726a245d7f404c0ab9a0adcf294aa17fd0
Isaiah Miller <miller.isaiah.n@gmail.com>
2018-11-21 13:15:35 -0800

    adds Exportable and SettingsController. Does not implement SettingsController

---
+++ KittenProject/src/application/SettingsController.java
@@ -0,0 +1,9 @@
+package application;
+/*
+ * should contain an instance of Settings
+ * Should contain an instance of FileChooserDIY which it will pass 
+ * the settings instance to
+ */
+public class SettingsController {
+
+}
